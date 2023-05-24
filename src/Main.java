0ddeb0e2877ebf0a57c19b9433eaf3f024c81b0c
rakinplaban <rakinshahriar54@gmail.com>
2023-05-25 00:02:41 +0600

    Working on main.

--- src/Main.java
+++ src/Main.java
@@ -15,10 +15,29 @@ public class Main {
 		
 		SocialMediaApp my_app = new SocialMediaApp();
 		Scanner input = new Scanner(System.in);
-		String username = input.nextLine();
-		String password = input.nextLine();
-		RegularUser user = new RegularUser(username,password);
+//		String username = input.nextLine();
+//		String password = input.nextLine();
+//		RegularUser user = new RegularUser(username,password);
 		
-		my_app.addUser(null);
+		System.out.println("Please, choose an option");
+		System.out.print("1--- For adding new user.\n"
+				+ "2--- For romoving a user.\n"+
+				"3--- Display the profile information of all users.\n"+
+				"4--- Add posts to regular user accounts\n"+"5--- Display the posts of a specific user.\n"+
+				"6--- Like posts.\n"+"7--- Unlike posts.\n"+"Any key to terminate.\n");
+		
+		int choice = input.nextInt();
+		while(true) {
+			switch(choice) {
+			case 1:
+				String username = input.nextLine();
+				String password = input.nextLine();
+				User user = new RegularUser(username,password);
+				my_app.addUser(user);
+			}
+		}
+		
+		
+		
 	}
 }
